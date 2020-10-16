a7a8e6ce0cbbd14789c96edded053bc850e00c0c
Nanu Miah <nanumiah@nanus-mbp.home>
2020-10-17 00:39:04 +0100

    start of app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Accordian from './components/Accordian'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React',
+        content: 'React isd a favourite JS libary among enginners for its speed'
+    },
+    {
+        title: 'How do you use REACT',
+        CONTENT: 'You use React by creating components'
+    }
+];
 
-export default App;
+export default () => {
+    return (
+        <div>
+            <Accordian />
+        </div>
+        );
+};
---
+++ src/components/Accordian.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Accordian = () => {
+    return <h1>Accordian</h1>
+};
+
+export default Accordian;
